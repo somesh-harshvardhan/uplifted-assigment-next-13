d9dbdd06ab8630a4327f6ab50a1b1c69de378e9b
somesh-harshvardhan <somesh.harshvardhan@gmail.com>
2023-10-13 01:29:33 +0530

    speedometer done

--- app/quiz/ScoreScreen.js
+++ app/quiz/ScoreScreen.js
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import OverlappingLayer from "../components/assets/OverlappingLayer";
 import Button from "../components/shared/button/Button";
+const SpeedoMeter = ({ total, score }) => {
+  const rotate = (score / total) * 180 + 270;
+  const result = (score / total) * 100;
+  return (
+    <div className=" h-[230px] overflow-hidden">
+      <div className=" h-[200px] w-full flex items-center justify-center relative ">
+        <svg width="200" height="200" viewBox="0 0 500 260" fill="none">
+          <path
+            d="M490 250C490 117.452 382.548 10 250 10C117.452 10 10 117.452 10 250"
+            stroke="url(#paint0_linear_86_5)"
+            strokeWidth="20"
+            strokeLinecap="round"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear_86_5"
+              x1="490"
+              y1="10"
+              x2="10"
+              y2="10"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#44B77B" />
+              <stop offset="0.479043" stopColor="#FFD033" />
+              <stop offset="1" stopColor="#FF3B3F" />
+            </linearGradient>
+          </defs>
+        </svg>
+        <div className=" absolute bg-[#EBEDF5] h-[184px] w-[184px] rounded-full top-[56px] z-10"></div>
+        <div
+          className="absolute bg-white h-[120px] w-[120px] rounded-full top-[87px] z-50 transition-[rotate] duration-1000 shadow-md"
+          style={{ rotate: rotate + "deg" }}
+        >
+          <div className=" static z-10 h-full w-full bg-white rounded-full">
+            <div className=" absolute -top-[1px] left-1/2 -translate-x-1/2 rotate-[49deg] skew-y-[29deg] skew-x-[33deg] h-[30px] w-[30px] bg-black -z-10"></div>
+            <div className=" absolute z-30 h-[100px] w-[100px] border border-slate-200 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full"></div>
+          </div>
+        </div>
+        <div className=" h-[184px] w-[184px] absolute bg-white rotate-45 z-20 top-[150px]"></div>
+        <div className=" absolute z-50 top-[145px] left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl font-bold">
+          {result} %
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const ScoreScreen = ({ score, questions, startQuiz }) => {
   return (
@@ -9,6 +55,7 @@ const ScoreScreen = ({ score, questions, startQuiz }) => {
 
       <div className=" bg-white w-full flex-1 rounded-tr-[52px] rounded-tl-[52px] relative p-5">
         <h3 className=" mt-5 font-bold text-2xl text-center">Your Result</h3>
+        <SpeedoMeter total={questions.length} score={score} />
         <div className=" mt-9">
           <div>
             <li className=" list-none  bg-green-1/[.1] rounded-lg min-h-12 flex items-center py-4 px-4 gap-x-2 text-sm">
@@ -25,9 +72,9 @@ const ScoreScreen = ({ score, questions, startQuiz }) => {
             </li>
           </div>
         </div>
-      </div>
-      <div className=" fixed bottom-[50px] w-[calc(100%-40px)] left-1/2 -translate-x-1/2 max-w-md">
-        <Button label={"Start Again"} onClick={startQuiz} />
+        <div className=" mt-9">
+          <Button label={"Start Again"} onClick={startQuiz} />
+        </div>
       </div>
     </div>
   );
--- app/quiz/page.js
+++ app/quiz/page.js
@@ -16,6 +16,7 @@ const Quiz = () => {
 
   const startQuiz = async () => {
     await fetchQuestions();
+    setScore(0);
     setStart(QUIZ_STATES.quiz);
   };
 
@@ -29,7 +30,7 @@ const Quiz = () => {
         setloading(true);
         const res = await fetch("/api/quiz-data", { method: "GET" });
         const data = await res.json();
-        setQuestions([...data.quiz.questions]);
+        setQuestions([...data.quiz.questions].slice(0, 5));
       } catch (error) {
         console.log(error);
       } finally {
